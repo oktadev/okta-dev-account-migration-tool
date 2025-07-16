6f83c29f3e85dae2e1ba1fb73d5ef9b070ce2d0a
E. Dunham <github@edunham.net>
2025-07-16 14:02:30 -0700

    regexp: now they're called integrator not trial

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func PrepareOktaCliArgs(cfg *Config, args ...string) []string {
 	return args
 }
 
-var devOrgPattern = regexp.MustCompile(`(?i)((?:dev|trial)-\d+)\.okta`)
+var devOrgPattern = regexp.MustCompile(`(?i)((?:dev|trial|integrator)-\d+)\.okta`)
 
 func LoadConfig(configPath string) (*Config, error) {
 	if configPath == "" {
